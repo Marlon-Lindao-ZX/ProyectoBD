c7b497ce5c5828395182d82574811a70c50d856e
marln <melindao@espol.edu.ec>
2020-12-04 22:38:00 -0500

    Añadiendo ultimos cambios

--- Grafica.py
+++ Grafica.py
@@ -1,5 +1,6 @@
 import MapReduce
 import sys
+import getopt
 import re
 from pymongo import MongoClient
 import matplotlib.pyplot as plt
@@ -8,7 +9,9 @@ import pprint
 
 mr = MapReduce.MapReduce()
 
-latinoamerica = [
+continentes = ['europa', 'sudamerica']
+
+sudamerica = [
     'Argentina',
     'Bolivia',
     'Brazil',
@@ -78,11 +81,18 @@ europa = [
     'Uzbekistan',
 ]
 
-latino = {}
-edad_latino = {}
+total = {}
+edad_total = {}
 
-europe = {}
-edad_europe = {}
+validador = 'mundo'
+
+continente = 'all'
+
+pais = 'all'
+
+genero = 'all'
+
+generos = ['male','female']
 
 # =============================
 # Do not modify above this line
@@ -91,18 +101,32 @@ edad_europe = {}
 def mapper(record):
     # key: document identifier
     # value: document contents
-    #key = (record[2], record[4], record[1])
-    key = (record[2], record[1],record[4])
+    # key = (record[2], record[4], record[1])
+    key = (record[2], record[1], record[4])
     value = record[5]
-    #words = value.split()
+    # words = value.split()
 
     # ---- TU CODIGO AQUI ----
     # para cada palabra w en la lista 'words' emite  (w,1)
     # usa mr.emit_intermediate
     # ------------------------
-    #print('clave: ' + key)
-    #print('valor:' + value)
-    mr.emit_intermediate(key, value)
+    # print('clave: ' + key)
+    # print('valor:' + value)
+    if validador == 'europa' and record[1] in europa:
+        if genero != 'all' and record[3] == genero:
+            mr.emit_intermediate(key, value)
+        elif genero == 'all':
+            mr.emit_intermediate(key, value)
+    elif validador == 'sudamerica' and record[1] in sudamerica:
+        if genero != 'all' and record[3] == genero:
+            mr.emit_intermediate(key, value)
+        elif genero == 'all':
+            mr.emit_intermediate(key, value)
+    elif validador == 'mundo':
+        if genero != 'all' and record[3] == genero:
+            mr.emit_intermediate(key, value)
+        elif genero == 'all':
+            mr.emit_intermediate(key, value)
 
 
 def reducer(key, list_of_values):
@@ -115,54 +139,77 @@ def reducer(key, list_of_values):
     sum = 0
     for value in list_of_values:
         sum += int(value)
-    validate_sudamerica(key,sum)
-    validate_europa(key,sum)
+    validate_total(key, sum)
     mr.emit((key, sum))
 
 
-def validate_sudamerica(key, sum):
-    if key[1] in latinoamerica:
-        if key[0] in latino.keys():
-            latino[key[0]] += sum
-        else:
-            latino[key[0]] = sum
-        if key[2] in edad_latino.keys():
-            if key[0] in edad_latino[key[2]].keys():
-                edad_latino[key[2]][key[0]] += sum
-            else:
-                edad_latino[key[2]][key[0]] = sum
-        else:
-            edad_latino[key[2]] = {}
-            edad_latino[key[2]][key[0]] = sum
+def validate_total(key, sum):
+    if key[0] in total.keys():
+        total[key[0]] += sum
+    else:
+        total[key[0]] = sum
 
-def validate_europa(key, sum):
-    if key[1] in europa:
-        if key[0] in europe.keys():
-            europe[key[0]] += sum
+    if key[2] in edad_total.keys():
+        if key[0] in edad_total[key[2]].keys():
+            edad_total[key[2]][key[0]] += sum
         else:
-            europe[key[0]] = sum
-        if key[2] in edad_europe.keys():
-            if key[0] in edad_europe[key[2]].keys():
-                edad_europe[key[2]][key[0]] += sum
+            edad_total[key[2]][key[0]] = sum
+    else:
+        edad_total[key[2]] = {}
+        edad_total[key[2]][key[0]] = sum
+
+
+def manage_options(argv):
+    global validador
+    global pais
+    global genero
+
+    try:
+      opts, args = getopt.getopt(argv, "hc:p:g:", ["continente=", "pais=","genero="])
+    except getopt.GetoptError:
+      print('Uso: Grafica.py [-c <continente>] [-p <pais>] [-g <genero>] [-h]')
+      sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('Grafica.py [-c <continente>] [-p <pais>] [-g <genero>] [-h]')
+            sys.exit()
+        elif opt in ("-c", "--continente"):
+            if arg in continentes:
+                validador = arg
             else:
-                edad_europe[key[2]][key[0]] = sum
-        else:
-            edad_europe[key[2]] = {}
-            edad_europe[key[2]][key[0]] = sum
+                print('Parametro enviado no es valido, utilizando valor mundo para trabajar con todos los datos')
+        elif opt in ("-p", "--pais"):
+            pais = arg
+            print('Opcion p activado, se ignorará opcion c')
+        elif opt in ("-g", "--genero"):
+            if arg in generos:
+                genero = arg
+            else:
+                print('Parametro enviado no es valido, utilizando valor all para trabajar con todos los datos')
 
 
 # Do not modify below this line
 # =============================
 if __name__ == '__main__':
-    #inputdata = open(sys.argv[1])
-    #mr.execute(inputdata, mapper, reducer)
+    manage_options(sys.argv[1:])
+    
+    # mr.execute(inputdata, mapper, reducer)
     lista = []
     client = MongoClient(
         "mongodb+srv://sDsVuNPCSUTtObcH:sDsVuNPCSUTtObcH@cluster0.rjqka.mongodb.net/test?authSource=admin&replicaSet=atlas-zmesu9-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true")
     db = client.ProyectoBD
     # serverStatusResult = db.command("serverStatus")}
 
-    resultado = db.Suicidios.find({'year': {'$gt': '1999', '$lt':'2016'}})
+    if pais != 'all':
+        validador = 'mundo'
+        resultado = db.Suicidios.find({'year': {'$gt': '1999', '$lt':'2016'}, 'country': pais})
+    else:
+        resultado = db.Suicidios.find({'year': {'$gt': '1999', '$lt':'2016'}})
+
+    if resultado is None:
+        print("La consulta a la base no arrojo resultados, terminando programa")
+        sys.exit()
+
     for object in resultado:
         # print(object.values())
         listaSTR = "["
@@ -179,34 +226,39 @@ if __name__ == '__main__':
 
     mr.execute(lista, mapper, reducer)
 
-    plt.figure(figsize=(20,10))
-    plt.plot(latino.keys(),latino.values())
-    plt.savefig('latino.png')
+    filename = message = validador
 
-    print('Generado grafica de numeros de suicidios en Sudamérica desde el año 2000 al 2015')
-    print('Grafica guardada en latino.png')
-
-    plt.figure(figsize=(20,10))
-    for edad in edad_latino:
-        plt.plot(edad_latino[edad].keys(),edad_latino[edad].values())
-    plt.savefig('RangoEdad_latino.png')
-
-    print('Generado grafica de numeros de suicidios por rango de edad en Sudamérica desde el año 2000 al 2015')
-    print('Grafica guardada en RangoEdad_latino.png')
+    if pais != 'all':
+        filename = pais
+        message = 'pais ' + pais
     
-    plt.figure(figsize=(20,10))
-    plt.plot(europe.keys(),europe.values())
-    plt.savefig('europe.png')
-
-    print('Generado grafica de numeros de suicidios en Europa desde el año 2000 al 2015')
-    print('Grafica guardada en europe.png')
+    if genero != 'all':
+        filename += '_' + genero
+        message += ' buscando por genero \'' + genero + '\'' 
 
     plt.figure(figsize=(20,10))
-    for edad in edad_europe:
-        plt.plot(edad_europe[edad].keys(),edad_europe[edad].values())
-    plt.savefig('RangoEdad_europa.png')
+    x = []
+    y = []
+    for key in sorted(total.keys()):
+        x.append(key)
+        y.append(total[key])
+    plt.plot(x,y)
+    plt.savefig(filename+'.png')
 
-    print('Generado grafica de numeros de suicidios por rango de edad en Europa desde el año 2000 al 2015')
-    print('Grafica guardada en RangoEdad_europa.png')
+    print('Generado grafica de numeros de suicidios en el '+ message  +' desde el año 2000 al 2015')
+    print('Grafica guardada en ' + filename + '.png')
+
+    plt.figure(figsize=(20,10))
+    for edad in edad_total:
+        x = []
+        y = []
+        for key in sorted(edad_total[edad].keys()):
+            x.append(key)
+            y.append(edad_total[edad][key])
+        plt.plot(x,y)
+    plt.savefig('RangoEdad_'+ filename+'.png')
+
+    print('Generado grafica de numeros de suicidios por rango de edad en el '+ message +' desde el año 2000 al 2015')
+    print('Grafica guardada en RangoEdad_'+ filename+'.png')
 
     
--- image.sh
+++ image.sh
@@ -2,4 +2,25 @@
 
 clear
 rm *.png
-python3 Grafica.py
+echo "\nHaciendo normal\n"
+python3 Grafica.py -c europa
+echo "\n"
+python3 Grafica.py -c sudamerica
+echo "\n"
+python3 Grafica.py -c mundo
+
+echo "\nHaciendo con pais\n"
+python3 Grafica.py -c europa -p Ecuador
+echo "\n"
+python3 Grafica.py -p Spain
+echo "\n"
+python3 Grafica.py -p Japan
+
+echo "\nHaciendo por genero\n"
+python3 Grafica.py -c sudamerica -g male
+echo "\n"
+python3 Grafica.py -p Ecuador -g female
+echo "\n"
+python3 Grafica.py -c mundo -g male
+echo "\n"
+python3 Grafica.py -c europa -g female
